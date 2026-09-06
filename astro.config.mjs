9686d75ffc8750bdd7cc6fe17f444f9806b1ff2b
george-taschina <georgecosmintaschina1631@gmail.com>
2026-09-06 15:29:54 +0200

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://polisportivafresonara.it',
   integrations: [sitemap()],
+  adapter: netlify(),
 });
-
